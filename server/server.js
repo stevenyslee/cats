13779fb624fb7b6326f397f95b93964c2da9cae2
Steven Lee <stevenyslee@gmail.com>
2018-12-13 15:02:11 -0800

    Setup endpoints

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/cats', (req, res) => res.send('Hello World!'));
+
+app.post('/cat/register', (req, res) => res.send('Hello World!'));
+
+app.put('/cat/login', (req, res) => res.send('Hello World!'));
+
+app.get('/cats/random', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('App listening on port 3000'));
