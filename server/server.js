8da139be27155651eeacb3c5f76f890fa6164a15
Steven Lee <stevenyslee@gmail.com>
2018-12-14 00:30:05 -0800

    Respond with object

--- server/server.js
+++ server/server.js
@@ -14,15 +14,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/cat/register', (req, res) => {
-  if (!req.body.name) return res.status(400).send('Name invalid');
-  if (!req.body.username) return res.status(400).send('Username invalid');
-  if (req.body.password < 8) return res.status(400).send('Password must be longer than 8 characters');
+  if (!req.body.name) return res.status(400).send({ error: 'Name invalid' });
+  if (!req.body.username) return res.status(400).send({ error: 'Username invalid'});
+  if (req.body.password < 8) return res.status(400).send({ error: 'Password must be longer than 8 characters'});
 
   bcrypt.hash(req.body.password, saltRounds, (hashErr, hash) => {
-    if (hashErr) return res.status(500).send(hashErr);
+    if (hashErr) return res.status(500).send({ error: hashErr });
     req.body.password = hash;
     registerCat(req.body, (err, response) => {
-      if (err) return res.status(500).send(err);
+      if (err) return res.status(500).send({ error: err });
       return res.status(200).send(response);
     });
   });
@@ -31,13 +31,13 @@ app.post('/cat/register', (req, res) => {
 
 app.put('/cat/login', (req, res) => {
   checkPassword(req.body, (err, response) => {
-    if (err) return res.status(500).send(err);
-    if (!response.length) return res.status(400).send('No such username');
+    if (err) return res.status(500).send({ error: err });
+    if (!response.length) return res.status(400).send({ error: 'No such username' });
 
     bcrypt.compare(req.body.password, response[0].password, (err, resHash) => {
-      if (err) return res.status(400).send('Password Incorrect');
+      if (err) return res.status(400).send({ error: 'Password Incorrect'});
       updateLastDate(req.body.username, (err, resDate) => {
-        if (err) return res.status(500).send(err);
+        if (err) return res.status(500).send({ error: err });
         let token = jwt.sign({ username: req.body.username, password: response[0].password }, cert, { algorithm: 'HS256', expiresIn: 86400 }, (err, token) => {
           return res.status(200).send({ auth: true, token });
         });
@@ -60,7 +60,7 @@ app.get('/cats', (req, res) => {
 
 app.get('/cats/random', (req, res) => {
   randomCat((err, response) => {
-    if (err) return res.status(500).send(err);
+    if (err) return res.status(500).send({ error: err });
     return res.status(200).send(response);
   });
 });
