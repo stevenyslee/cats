132a132824aa7f260f0796f204383ad431970f7f
Steven Lee <stevenyslee@gmail.com>
2018-12-13 16:15:46 -0800

    Add queries methods

--- database/index.js
+++ database/index.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 const { host, user, password, database } = require('./config.js');
 
-const connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host     : host,
   user     : user,
   password : password,
   database : database
 });
 
-const selectAll = (callback) => {
-  connection.query('SELECT * FROM items', (err, results, fields) => {
+const registerCat = ({ addedAt, breed, birthdate, imageUrl, lastSeenAt, name, username, password, weight }, callback) => {
+  connection.query(`INSERT INTO cats (addedAt, breed, birthdate, imageUrl, lastSeenAt, name, username, password, weight) VALUES ('${addedAt}', '${breed}', '${birthdate}', '${imageUrl}', '${lastSeenAt}', '${name}', '${username}', '${password}', ${weight})`, (err, results, fields) => {
     if(err) {
       callback(err, null);
     } else {
@@ -18,4 +18,17 @@ const selectAll = (callback) => {
   });
 };
 
-module.exports.selectAll = selectAll;
+const randomCat = (callback) => {
+  connection.query(`SELECT imageUrl, name, breed FROM cats ORDER BY RAND() LIMIT 1;`, (err, results, fields) => {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+module.exports = {
+  registerCat,
+  randomCat
+};
