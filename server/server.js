51a951353867aa8359f196c39c51df4251e60338
Steven Lee <stevenyslee@gmail.com>
2018-12-14 00:44:07 -0800

    Allow CORS

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
